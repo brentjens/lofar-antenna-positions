d02c1a93b11ee2b4e63f8d5436c4f256c61ed4c4
Michiel Brentjens <brentjens@astron.nl>
2016-06-06 11:20:11 -0600

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from lofarantpos import __version__
+
+setup(name='lofar-antenna-positions',
+      version      = __version__,
+      description  = 'Access, query, and manipulate LOFAR antenna positions from ',
+      author       = 'M.A. Brentjens',
+      author_email = 'brentjens@astron.nl',
+      packages     = ['lofarantpos'],
+      requires     = ['numpy'],
+      scripts      = [],
+      data_files   = [('share/lofarantpos/',
+                       ['share/lofarantpos/etrs-antenna-positions.csv',
+                        'share/lofarantpos/etrs-phase-centres.csv',
+                        'share/lofarantpos/hba-rotations.csv',
+                        'share/lofarantpos/normal_vectors.dat',
+                        'share/lofarantpos/rotation_matrices.dat'])]
+     )
+
